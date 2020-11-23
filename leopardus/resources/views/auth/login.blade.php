afc8e0090bfd3000e8a9f3003584785bb66e422b
Kriptza <Kriptza@gmail.com>
2020-11-23 12:38:23 -0400

    Arreglo en vista movil

--- leopardus/resources/views/auth/login.blade.php
+++ leopardus/resources/views/auth/login.blade.php
@@ -1,32 +1,46 @@
 @extends('layouts.login')
 @section('content')
-<section class="row flexbox-container">
-    <div class="col-xl-8 col-11 d-flex justify-content-center">
+
+<section class="row flexbox-container box-shadow" >
+
+<div class="container">      
+  <div class="row">
+
+        <div class="col-md-3 d-flex justify-content-center ">
+        </div>
+
+        <div class="col-md-3 d-flex justify-content-center ">
+
+           <img src="{{asset('assets/imgLanding/logo2.png')}}" alt="" class=" m-auto branding logo" height="130" width="130">
+        </div>
+       
+            
+
+    <div class="col-md-4 col-11 d-flex justify-content-center">
         <div class="card bg-authentication rounded-0 mb-0" style="background: transparent;">
-            <div class="row m-0">
-                <div class="col-12 p-0">
-                    <div class="card rounded-0 mb-0 px-2" style="background: transparent;">
-                        <img src="{{asset('assets/imgLanding/logo2.png')}}" alt="" class="m-auto branding logo" height="130" width="130">
-                        <div class="card-header pb-1">
-                            <div class="card-title inicio m-auto">
-                                <h4 class="mb-0 text-white">Sesion</h4>
-                            </div>
-                            <div class="card-title recuperar" style="display:none;">
-                                <h4 class="mb-0 text-white">Recuperar tu clave</h4>
-                            </div>
-                        </div>
+            <div class="card rounded-3 mb-0 px-2" style="background: transparent;">
+                 
+                 {{--Cabecera --}}
+                 <div class="card-header pb-1">
+                  <div class="card-title text-left">
+                     <h4 class="mb-0 text-white text-left"> INICIAR SESIÓN</h4>
+                   </div>
+            
+                  <div class="card-title recuperar" style="display:none;">
+                     <h4 class="mb-0 text-white">Recuperar tu clave</h4>
+                  </div>
+              </div>
                         {{-- alertas --}}
                         @include('dashboard.componentView.alert')
 
-                        {{-- <p class="px-2">Welcome back, please login to your account.</p> --}}
-                        <div class="card-content">
+                        <div class="card-content breadcrumb">
                             <div class="card-body pt-1">
                                 {{-- registro --}}
                                 <form class="login-form inicio" method="POST"
                                     action="{{ route('autenticacion-login') }}">
                                     {{ csrf_field() }}
-                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
-                                        <input type="text" class="form-control" id="user-name" placeholder="Username"
+                                    <fieldset class="form-label-group form-group position-relative has-icon-left ">
+                                        <input type="text-white" class="form-control btn-outline-primary" id="user-name" placeholder="Usuario"
                                             required value="{{ old('user_email') }}" name="user_email">
                                         <div class="form-control-position">
                                             <i class="feather icon-user"></i>
@@ -35,8 +49,8 @@
                                     </fieldset>
 
                                     <fieldset class="form-label-group position-relative has-icon-left">
-                                        <input type="password" class="form-control" id="user-password"
-                                            placeholder="Password" required name="password">
+                                        <input type="password" class="form-control btn-outline-primary" id="user-password"
+                                            placeholder="Contraseña" required name="password" style="placeholder">
                                         <div class="form-control-position">
                                             <i class="feather icon-lock"></i>
                                         </div>
@@ -56,20 +70,22 @@
                                                 </div>
                                             </fieldset>
                                         </div>
-                                        <div class="text-right">
+                                        
+                                    </div><button type="submit" class="btn btn-primary float-right btn-inline col-12 mt-2"> CONECTAR
+                                    </button>
+                                    {{-- <div class="row">
+                                        <a href="{{route('autenticacion.new-register')}}"
+                                        class="btn btn-outline-primary float-left btn-inline col-12">Registro</a> 
+                                    </div> --}}
+                                    <div class="text-left">
                                             <a class="card-link" onclick="toggle()" href="javascript:;">
                                                 ¿Olvidaste tu Clave?
                                             </a>
+                                       
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <a href="{{route('autenticacion.new-register')}}"
-                                        class="btn btn-outline-primary float-left btn-inline col-12">Registro</a>
-                                    <button type="submit" class="btn btn-primary float-right btn-inline col-12 mt-2">Iniciar Sesión</button>
-                                    </div>
-                                    
+
                                 </form>
-                                {{-- reset password --}}
+                                {{-- reset password 
                                 <form class="forget-form recuperar" action="{{route('autenticacion.clave')}}"
                                     method="post" style="display:none;">
                                     {{ csrf_field() }}
@@ -84,27 +100,29 @@
                                     <div class="float-md-left d-block mb-1 col-12">
                                         <a href="javascript:;" class="btn btn-outline-primary btn-block px-75"
                                             onclick="toggle()">Regresar al Login</a>
+                                    
                                     </div>
                                 </form>
-                            </div>
+                                --}}
+
                         </div>
-                        <hr>
-                        {{-- <div class="login-footer">
-                            <div class="divider">
-                                <div class="divider-text">OR</div>
-                            </div>
-                            <div class="footer-btn d-inline">
-                                <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
-                                <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
-                                <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
-                                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
-                            </div>
-                        </div> --}}
-                    </div>
-                </div>
             </div>
         </div>
     </div>
+      </div>
+
+
+
+</div>
+<div class="col-md-3 d-flex justify-content-center ">
+        </div>
+
+
+<div class=" col-md-12 text-right">
+    <button type="button " class="btn btn-icon rounded-circle btn-default bg-dark text-white ">
+        <i class="font-medium-5 fa fa-paper-plane-o mr-50"></i> 
+    </button>
+</div>
 </section>
 
 <script type="text/javascript">
