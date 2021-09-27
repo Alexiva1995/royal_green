142ec7fa79f81e56f021740c5643509d20820ab8
eulinp <eulinpr@gmail.com>
2021-09-27 19:43:48 -0400

    fix bono binario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         //$this->call(UserSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(PackagesTableSeeder::class);
-        // $this->call(OrdenPurchaseTableSeeder::class);
+        $this->call(OrdenPurchaseTableSeeder::class);
         $this->call(RanksTableSeeder::class);
         // $this->call(CountryTableSeeder::class);
         // $this->call(CategoriesTableSeeder::class);
