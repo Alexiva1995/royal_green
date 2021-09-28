9b8ab4b0d22992e4dc946a42ad5cdba3c69515bc
cesar899 <montevideocesar599@gmail.com>
2021-09-28 14:12:19 -0400

    historial de puntos

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -9,6 +9,7 @@ use App\Models\Inversion;
 use App\Models\RankRecords;
 use App\Models\WalletBinary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class AuditController extends Controller
@@ -58,8 +59,10 @@ class AuditController extends Controller
 
     public function puntosBinarios()
     {
+         // Liquidaction::where([['iduser', '=', Auth::user()->id], ['status', '=', '1']])->get();
+          $puntos = WalletBinary::where('iduser', '=', Auth::user()->id)->orderBy('id', 'desc')->get();
         try {
-             return view('audit.puntos');
+             return view('audit.puntos', compact('puntos'));
          } catch (\Throwable $th) {
              Log::error('AuditController - index -> Error: '.$th);
              abort(403, "Ocurrio un error, contacte con el administrador");
