6afbba3edf4d0f91f32b1145fab148ac44db1c9d
eulinp <eulinpr@gmail.com>
2021-09-28 13:31:58 -0400

    Fix Pay Puntos Binarios

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -458,7 +458,7 @@ class WalletController extends Controller
                                     ])->first();
                                     if (empty($check)) {
                                         $concepto = 'Puntos binarios del Usuario '.$orden->getOrdenUser->fullname;
-                                        $puntosD = $puntosI = 0;
+                                        $puntos_reales = $puntosD = $puntosI = 0;
                                         if ($sponsor->status == '1') {
                                             if ($side == 'I') {
                                                 $puntosI = $orden->total;
@@ -468,19 +468,21 @@ class WalletController extends Controller
                                                 $puntos_reales = $puntosD;
                                             }
                                         }
-                                        $dataWalletPoints = [
-                                            'iduser' => $sponsor->id,
-                                            'referred_id' => $orden->iduser,
-                                            'orden_purchase_id' => $orden->id,
-                                            'puntos_i' => $puntosI,
-                                            'puntos_d' => $puntosD,
-                                            'puntos_reales' => $puntos_reales,
-                                            'side' => $side,
-                                            'status' => 0,
-                                            'descripcion' => $concepto
-                                        ];
+                                        if($puntos_reales != 0){
+                                            $dataWalletPoints = [
+                                                'iduser' => $sponsor->id,
+                                                'referred_id' => $orden->iduser,
+                                                'orden_purchase_id' => $orden->id,
+                                                'puntos_i' => $puntosI,
+                                                'puntos_d' => $puntosD,
+                                                'puntos_reales' => $puntos_reales,
+                                                'side' => $side,
+                                                'status' => 0,
+                                                'descripcion' => $concepto
+                                            ];
+                                            WalletBinary::create($dataWalletPoints);
+                                        }
                                         
-                                        WalletBinary::create($dataWalletPoints);
                                 }
                         }                    
                         }
@@ -503,7 +505,7 @@ class WalletController extends Controller
                                 ])->first();
                                 if (empty($check)) {
                                     $concepto = 'Puntos binarios del Usuario '.$orden->getOrdenUser->fullname;
-                                    $puntosD = $puntosI = 0;
+                                    $puntosReales = $puntosD = $puntosI = 0;
                                     if ($sponsor->status == '1') {
                                         if ($side == 'I') {
                                             $puntosI = $orden->total;
